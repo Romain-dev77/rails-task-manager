9627c43066229f81526a9b8f9cd0a451047578ca
Romain Gateaud <romaingateaud@hotmail.com>
2026-02-24 16:40:55 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  delete "tasks/:id", to: "tasks#destroy"
 end
